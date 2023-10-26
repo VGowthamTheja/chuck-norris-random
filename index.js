0ce466bac0e55b542ebc9449f36dc0482aaeecaf
Vempalli Gowtham Theja <vempalligowthamtheja@gmail.com>
2023-10-26 14:27:52 +0000

    first package commit

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
-const url = "https://api.chucknorris.io/jokes/random"
+#!/usr/bin/env node
+
+let joke;
+const url = "https://api.chucknorris.io/jokes/random?exclude=explicit"
 fetch(url, {
     method: 'GET',
     headers: {
         'Content-Type': 'application/json'
     }
-}).then(res => res.json()).then(data => console.log(data)).catch(err => console.log(err));
+}).then(res => res.json()).then(data => {
+    joke = data
+    console.log(data.value);
+}).catch(err => console.log(err));
+
+
+module.exports = {
+    url: url,
+    joke: joke
+}
